cad73f732f3638878d2b5d9b1c8f2aa57e73f74a
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-17 09:09:08 +0100

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,18 @@
+// Only change code below this line
+function inverseWhile() {
+    var fiveNumbers = "";
+    var i = 5;
+    while (i >= 0) {
+        fiveNumbers += i;
+        i--
+        if (i >= 0) {
+            fiveNumbers += ",";
+        }
+    }
+    return fiveNumbers;
+}
 
+inverseWhile();
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
