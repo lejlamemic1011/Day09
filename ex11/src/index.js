1b9354d656b903586e74344fb1386162d83c00e4
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-17 09:07:50 +0100

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,29 @@
+function switchCase(letter) {
+    // Write code below this line
+    switch (letter) {
+        case "a":
+            animals = "antelope";
+            break;
 
+        case "b":
+            animals = "bird";
+            break;
+
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";
+    }
+
+    // Write code above this line
+    return animals;
+}
+
+console.log(switchCase("a")); // Change this line
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
+
+module.exports = switchCase;
